c251891ab4f290f284d358f2850b2f4b206f5655
Mark <mark@example.dev>
2021-12-14 19:34:49 -0800

    CD methods, start on sync update method

--- backend/__tests__/models/farm.test.ts
+++ backend/__tests__/models/farm.test.ts
@@ -3,7 +3,7 @@
 
 import Farm from "../../models/farm";
 import { commonAfterAll, commonAfterEach, commonBeforeAll, commonBeforeEach } from "./_testCommon";
-import { NotFoundError } from "../../expressError";
+import { BadRequestError, NotFoundError } from "../../expressError";
 import db from "../../db";
 
 beforeAll(commonBeforeAll);
@@ -79,3 +79,117 @@ describe("Get by owner method" , () => {
     }
   });
 });
+
+describe("Create method", () => {
+  it("works", async () => {
+    const q = await db.query("SELECT id FROM geo_profiles");
+    const { id:location } = q.rows[0];
+    const res = await Farm.create({ owner: "u1", location });
+    expect(res).toEqual({
+      id: expect.any(Number),
+      width: 3, 
+      length: 3,
+      lastCheckedAt: expect.any(Date),
+      irrigationLVL: 0,
+      locationID: location,
+      owner: "u1"
+    });
+  });
+
+  it("throws BadRequestError if invalid owner username", async () => {
+    const q = await db.query("SELECT id FROM geo_profiles");
+    const { id } = q.rows[0];
+    try {
+      await Farm.create({owner: "idontexist", location: id});
+      fail();
+    } catch (err) {
+      expect(err).toBeInstanceOf(BadRequestError);
+    }
+  });
+
+  it("throws BadRequestError if invalid location id", async () => {
+    try {
+      await Farm.create({owner: "u3", location: -1});
+      fail();
+    } catch (err) {
+      expect(err).toBeInstanceOf(BadRequestError);
+    }
+  });
+});
+
+describe("Delete method", () => {
+  it("works", async () => {
+    const q = await db.query("SELECT id FROM farms");
+    const { id } = q.rows[0];
+    const res = await Farm.delete(id);
+    expect(res).toEqual({ deleted: id });
+    // changes reflect in db
+    const q2 = await db.query("SELECT * FROM farms WHERE id = $1", [id]);
+    expect(q2.rowCount).toEqual(0);
+  });
+});
+
+describe("date match test", () => {
+  it("works", async () => {
+    const today = new Date();
+    const q = await db.query("SELECT id FROM geo_profiles");
+    const { id: location } = q.rows[0];
+    const q2 = await db.query(
+      `INSERT INTO weather_data (location, date, avg_temp, avg_cloud, total_rainfall)
+       VALUES ($1, $2, 20, 20, 20)`, [location, today]
+    );
+    const q3 = await db.query("SELECT location, date FROM weather_data WHERE location = $1 AND date = $2", [location, today]);
+    expect(q3.rows[0].date).toEqual(new Date(today.toDateString()));
+  });
+})
+
+// describe("Crop sync method", () => {
+
+//   it("updates crops", async () => {
+//     // Get snapshot of previous crop state
+//     const snapshot = await db.query("SELECT * FROM crops WHERE berry_type = 'cheri' ");
+//     // Set a control date for time delta operations
+//     await db.query('UPDATE farms SET last_checked_at = $1 WHERE id = $2',
+//       [new Date('2021-12-10T03:24:00'), snapshot.rows[0].farm_id]);
+    
+//     // Perform sync
+//     const res = await Farm.syncCrops(snapshot.rows[0].farm_id);
+
+//     // Returns object with updated crop data
+//     expect(res).toEqual({
+//       ...expectFarmObj, id: snapshot.rows[0].farm_id, owner: 'u1', crops: [
+//         {
+//           id: expect.any(Number),
+//           berryType: 'cheri',
+//           curGrowthStage: 0,
+//           moisture: expect.any(Number),
+//           health: expect.any(Number),
+//           plantedAt: expect.any(Date),
+//           x: expect.any(Number),
+//           y: expect.any(Number)
+//         }]
+//     });
+
+//     // Performs updates on crops
+//     const updated = await db.query("SELECT * FROM crops WHERE berry_type = 'cheri' ");
+//     expect(snapshot.rows[0].moisture).not.toEqual(updated.rows[0].moisture);
+//   });
+
+//   it("updates lastCheckedAt on farm", async () => {
+//     const q = await db.query("SELECT * FROM farms");
+//     const { id } = q.rows[0];
+//     await Farm.syncCrops(id);
+//     const u = await db.query("SELECT * FROM farms WHERE id = $1", [id]);
+//     expect(q.rows[0].last_checked_at).not.toEqual(u.rows[0].last_checked_at);
+//     expect(u.rows[0].last_checked_at).toBeInstanceOf(Date);
+//   });
+
+//   it("throws NotFoundError if id is invalid", async () => {
+//     try {
+//       await Farm.syncCrops(-1);
+//       fail();
+//     } catch (err) {
+//       expect(err).toBeInstanceOf(NotFoundError);
+//     }
+//   });
+// });
--- backend/models/farm.ts
+++ backend/models/farm.ts
@@ -1,21 +1,33 @@
 import db from "../db";
-import { NotFoundError } from "../expressError";
+import Crop from "./crop";
+import { BadRequestError, NotFoundError } from "../expressError";
 
-interface CropObject{
+interface CropObject {
   id: number, berryType: string, curGrowthStage: number,
   moisture: number, health: number, plantedAt: Date,
   x: number, y: number
 };
 
-interface FarmObject{
+interface FarmObject {
   id: number, length: number, width: number, irrigationLVL: number,
   owner: string, locationName: string, locationRegion: string,
   locationCountry: string, crops: Array<CropObject>,
   lastCheckedAt: Date
 }
 
+interface CreateFarmProps {
+  owner: string, location: number, length?: number, 
+  irrigationLVL?: number, width?: number
+}
+
+const DEFAULT_WIDTH = 3;
+const DEFAULT_LENGTH = 3;
+
 export default class Farm{
 
+  /** Get detail information on a farm with given id
+   *  Throws NotFoundError if no farm with such id
+   */
   static async get(farmID:number) : Promise<FarmObject>{
     const res = await db.query(
       `SELECT farms.id AS "id", farms.length, farms.width, farms.irrigation_lvl AS "irrigationLVL", 
@@ -45,20 +57,99 @@ export default class Farm{
       id, length, width, irrigationLVL, lastCheckedAt, owner,
       locationName, locationRegion, locationCountry, crops
     }
-
   }
 
+  /** Gets a list of farms owned by a given username.
+   *  Each object in list contains basic farm data (crops not included)
+   *  Throws NotFoundError if no such user
+   */
   static async getByOwner(username:string){
     const res = await db.query(
-      `SELECT farms.id AS "id", farms.length, farms.width, farms.irrigation_lvl AS "irrigationLVL",
-      farms.last_checked_at AS "lastCheckedAt", gp.name AS "locationName", gp.region AS "locationRegion",
-      gp.country as "locationCountry"
-      FROM users
-      LEFT JOIN farms ON farms.owner = users.username
-      LEFT JOIN geo_profiles gp ON gp.id = farms.location
-      WHERE users.username = $1;`, [username]);
+      `SELECT farms.id AS "id", farms.length, farms.width, 
+              farms.last_checked_at AS "lastCheckedAt", farms.irrigation_lvl AS "irrigationLVL",
+              gp.name AS "locationName", gp.region AS "locationRegion", gp.country as "locationCountry"
+       FROM users
+       LEFT JOIN farms ON farms.owner = users.username
+       LEFT JOIN geo_profiles gp ON gp.id = farms.location
+       WHERE users.username = $1;`, [username]);
     
       if (res.rowCount < 1) throw new NotFoundError(`No user with username ${username}`);
       return res.rows[0].id ? res.rows : [];
+  }
+
+  /** Deletes farm with given ID.
+   *  Throws NotFoundError if no farm with such id.
+   */
+  static async delete(farmID:number){
+    const res = await db.query(
+      `DELETE FROM farms WHERE id = $1 RETURNING id`, [farmID]
+    );
+    if (res.rowCount < 1) throw new NotFoundError(`No farm with id ${farmID}`);
+    return { deleted: farmID };
+  }
+
+  /** Creates farm */
+  static async create({owner, location, length=DEFAULT_LENGTH, width=DEFAULT_WIDTH, irrigationLVL=0}:CreateFarmProps){
+    try {
+      const res = await db.query(
+        `INSERT INTO farms (owner, location, length, width, irrigation_lvl)
+         VALUES ($1, $2, $3, $4, $5)
+         RETURNING id, length, width, irrigation_lvl AS "irrigationLVL",
+                   owner, location AS "locationID", last_checked_at AS "lastCheckedAt"`,
+        [owner, location, length, width, irrigationLVL]
+      );
+      return { ...res.rows[0] };
+    } catch (err:any) {
+      if (err.code && err.code === '23503'){
+        const msg = err.constraint === 'farms_owner_fkey' ? `Invalid username ${owner}` : `Invalid location id ${location}`;
+        throw new BadRequestError(msg);
+      }
+      throw err;
+    }
+  }
+
+  static async syncCrops(farmID:number){
+    // Query for required crop and berry data
+    const cropRes = await db.query(
+      `SELECT crops.id, crops.cur_growth_stage AS "curGrowthStage", crops.moisture, crops.planted_at AS "plantedAt",
+              bp.growth_time AS "growthTime", bp.dry_rate AS "dryRate", bp.ideal_temp as "idealTemp", bp.ideal_cloud AS "idealCloud",
+              farms.irrigation_lvl AS "irrigationLVL, farms.last_checked_at AS "lastCheckedAt", farms.location,
+              wd.avg_temp AS "avgTemp", wd.avg_cloud AS "avgCloud", wd.total_rainfall AS "totalRain"
+       FROM crops
+       JOIN berry_profiles bp ON bp.name = crops.berry_type
+       JOIN farms ON farms.id = crops.farm_id
+       JOIN weather_data wd ON farms.location = wd.location
+       WHERE farms.id = $1`, [farmID]
+    );
+
+    // Query for weather data on this farm's location
+    const weatherRes = await db.query(
+      `SELECT wd.date, wd.avg_temp AS "avgTemp", wd.avg_cloud AS "avgCloud", wd.total_rainfall AS "totalRainfall"
+       FROM weather_data wd
+       JOIN farms ON wd.location = farms.location
+       WHERE farms.id = $1`, [farmID]
+    );
+
+    // Clean query responses
+    const crops = cropRes.rows.map( ({id, curGrowthStage, moisture, plantedAt, growthTime, dryRate, idealTemp, idealCloud}) => {
+      return {
+        id, curGrowthStage, moisture: Number(moisture), plantedAt, growthTime,
+        dryRate: Number(dryRate), idealTemp: Number(idealTemp), idealCloud: Number(idealCloud)}
+    });
+    const weatherData = new Map();
+    weatherRes.rows.forEach( ({date, avgTemp, avgCloud, totalRain}) => {
+      weatherData.set(date, {
+        avgTemp: Number(avgTemp), avgCloud: Number(avgCloud), totalRain: Number(totalRain)
+      });
+    });
+    const { irrigationLVL, lastCheckedAt, location } = cropRes.rows[0];
+
+    const today = new Date(new Date().toDateString()); // Get today's date with no time (00:00:00)
+    const timeDelta = (Date.now() - lastCheckedAt.getTime()) * 0.001; // Convert ms to seconds
+
+    for (let crop of crops){
+      const newMoisture = await Crop.calcMoisture(timeDelta, {cropID: crop.id});
+      await Crop.update(crop.id, {moisture: newMoisture});
+    }
   }
 }
