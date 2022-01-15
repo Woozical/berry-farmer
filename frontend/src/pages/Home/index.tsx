1dd78e3f0c1c0f18848daf05acc267f8086d4a98
Mark <mark@example.dev>
2022-01-15 08:10:34 -0800

    more detailed home screen

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import GlobalContext from "../../GlobalContext"
 import { useContext, useEffect, useRef, useState } from "react";
 import BerryFarmerAPI from "../../BerryFarmerAPI";
 import StatTracker from "../../components/StatTracker";
+import FarmPreviewIMG from "../../media/farm-preview.png";
 import { Link } from "react-router-dom";
 import { titleCase } from "../../utils";
 import "./style.css";
@@ -49,5 +50,23 @@ export default function HomePage(){
         <Link to="/signup" className="btn btn-lg btn-primary col-sm-3 offset-sm-2 mt-2 ml-sm-2">Signup</Link>
       </div>
     }
+    <div className="row justify-content-center mt-4 py-2 align-items-top">
+      <div className="bg-white col-lg-5 col-md-6 border-sm-end border-md-end-0 border-sm-start border-top border-sm-bottom-0 border-md-bottom">
+        <img width="100%" src={FarmPreviewIMG} alt="BerryFarmer" />
+      </div>
+      <div className="bg-white col-lg-5 col-md-6 border-md-start-0 border-sm-start border-end border-sm-top-0 border-md-top border-bottom text-start pt-3">
+        <p><b>Berry Farmer</b> is a web application that seeks to emulate the process of growing berries in the Pokémon games. The twist?
+        Your berry farm is located somewhere in the real world! Each type of berry has a profile of ideal growing conditions, and the 
+        real world weather data of your farm's location will aid or hinder the growth of your berries.</p>
+        <p>
+          After registering for a free account, use your startup funds to purchase your first farm and some berries to plant. Be sure to check
+          a berry's profile by clicking on its icon to ensure that it will stay healthy when exposed to the weather patterns of your farm. Check up
+          on your berries every now and again to make sure they're adequately watered.
+        </p>
+        <p className="fs-7">
+          If you're in need of financial aid, feel free to reach out to me :]
+        </p>
+      </div>
+    </div>
   </main>)
 }
